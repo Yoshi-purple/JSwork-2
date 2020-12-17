a0ae2bb494ad2ad0cb760c7a18fc13ee9b3ef672
Yoshi-purple <n7ydahew@gmail.com>
2020-12-17 18:51:05 +0900

    整数の判定の書き換えと、前回の結果を削除する機能を実装

--- app.js
+++ app.js
@@ -6,54 +6,44 @@
   const fizz = document.getElementById('fizz');
   const buzz = document.getElementById('buzz');
   const btn = document.getElementById('btn');
-  const ul = document.querySelector('.result ul');
-  const result = document.querySelector('.result');
-  let nanLi = document.createElement('li');
+  const resultUl = document.getElementById('resultUl');
+  const result = document.getElementById('result');
+  const nanLi = document.createElement('li');
   nanLi.textContent = '整数値を入力してください';
-
-  function resetVal() {
-    fizz.value = '';
-    buzz.value = '';
-  }
 
   btn.addEventListener('click', (event) => {
     event.preventDefault();
-    nanLi.remove();
-
+    while (resultUl.firstChild) {
+      resultUl.removeChild(resultUl.firstChild);
+    }
     if (
       !Number.isInteger(fizz.value % buzz.value) ||
       fizz.value === '' ||
       buzz.value === ''
     ) {
-      ul.remove();
-
-      resetVal();
-      result.appendChild(nanLi);
+      resultUl.appendChild(nanLi);
       document.body.appendChild(result);
     } else if (Number.isInteger(fizz.value % buzz.value)) {
-      nanLi.remove();
-
       for (let i = 1; i <= 99; i++) {
         if (i % fizz.value === 0 && i % buzz.value === 0) {
           let resultLi = document.createElement('li');
           resultLi.textContent = `FizzBuzz ${i}`;
-          ul.appendChild(resultLi);
+          resultUl.appendChild(resultLi);
         } else if (i % fizz.value === 0) {
           let resultLi = document.createElement('li');
 
           resultLi.textContent = `Fizz ${i}`;
-          ul.appendChild(resultLi);
+          resultUl.appendChild(resultLi);
         } else if (i % buzz.value === 0) {
           let resultLi = document.createElement('li');
 
           resultLi.textContent = `Buzz ${i}`;
-          ul.appendChild(resultLi);
+          resultUl.appendChild(resultLi);
         }
       }
-      result.appendChild(ul);
+      result.appendChild(resultUl);
     }
 
     document.body.appendChild(result);
-    resetVal();
   });
 }
